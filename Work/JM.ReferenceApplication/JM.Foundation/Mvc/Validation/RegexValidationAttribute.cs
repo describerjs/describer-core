366ec073a0ee8ec517dd0583469c7add0a1234b4
Sebastian van Elten <sve@joinmedia.de>
2014-07-18 10:52:23 +0200

    Custom ValidationAttribute mit Frontendausgabe, Htmlhelper Extension zum Test

---
+++ Work/JM.ReferenceApplication/JM.Foundation/Mvc/Validation/RegexValidationAttribute.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace JM.Foundation.Mvc.Validation
+{
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple=true)]
+    public class RegexValidationAttribute : ValidationAttribute, IClientValidatable
+    {
+        string _regex = "";
+        bool _allowEmptyString = false;
+ 
+        public RegexValidationAttribute(string regex, bool allowEmptyString, string errorMessage)
+            : base(errorMessage)
+        {
+	        _regex = regex;
+	        _allowEmptyString = allowEmptyString;
+        }
+ 
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+	            if (value == null || String.IsNullOrEmpty(value.ToString()))
+	            {
+					if(_allowEmptyString)
+						return ValidationResult.Success;
+						
+					return new ValidationResult(ErrorMessageString);
+	            }
+				
+				var val = value.ToString();
+
+				if(!Regex.IsMatch(val, _regex))
+					return new ValidationResult(ErrorMessageString);
+            }
+            catch (Exception ex)
+            {
+                // Do stuff, i.e. log the exception
+                // Let it go through the upper levels, something bad happened
+                throw ex;
+            }
+ 
+            return ValidationResult.Success;;
+        }
+    
+		public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+		{
+			string errorMessage = ErrorMessageString;
+
+			var regexRule = new ModelClientValidationRule();
+			regexRule.ErrorMessage = errorMessage;
+			regexRule.ValidationType = "jmval"; // This is the name the jQuery adapter will use
+			regexRule.ValidationParameters.Add("pattern", _regex);
+			if(_allowEmptyString)
+				regexRule.ValidationParameters.Add("allowempty", _allowEmptyString);
+
+			yield return regexRule;
+		}
+	}
+}
---
+++ Work/JM.ReferenceApplication/JM.ReferenceApplication/Models/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace JM.ReferenceApplication.Models
+{
+	public static class Extensions
+	{
+		public static IHtmlString JMValidationMessageFor<TModel, TValue>(
+							 this HtmlHelper<TModel> html,
+							 Expression<Func<TModel, TValue>> expression, object htmlAttributes = null, string errorMessage = null)
+		{
+			//var x = ModelMetadata.FromLambdaExpression<TModel, TValue>(expression, html.ViewData);
+
+			var attributes = new RouteValueDictionary(htmlAttributes);
+			if (attributes.ContainsKey("class"))
+				attributes["class"] += " error-msg";
+			else
+				attributes.Add("class", "error-msg");
+
+			return html.ValidationMessageFor(expression, errorMessage, attributes);
+		}
+
+		public static IHtmlString JMTextBoxFor<TModel, TValue>(
+							 this HtmlHelper<TModel> htmlHelper,
+							 Expression<Func<TModel, TValue>> expression, object htmlAttributes = null)
+		{
+			if(htmlAttributes == null)
+				htmlAttributes = new Dictionary<string, object>();
+			
+			var content = htmlHelper.TextBoxFor(expression, htmlAttributes);
+
+			return MvcHtmlString.Create(content.ToString().Replace("data-val", "data"));
+		}
+	}
+}
