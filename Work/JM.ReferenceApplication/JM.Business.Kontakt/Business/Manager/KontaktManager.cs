71faa02d5596945c93c760045727e87a80855950
Sebastian van Elten <sve@joinmedia.de>
2014-07-24 16:17:06 +0200

    Versionierung Dateien JM.Business.Kontakt

---
+++ Work/JM.ReferenceApplication/JM.Business.Kontakt/Business/Manager/KontaktManager.cs
@@ -0,0 +1,51 @@
+﻿using Autofac.Extras.DynamicProxy2;
+using JM.Business.Kontakt.Business.Model;
+using JM.Business.Kontakt.Contracts.Manager;
+using JM.Business.Kontakt.Contracts.Model;
+using JM.Foundation;
+using JM.Foundation.Configuration;
+using JM.Foundation.ErrorHandling;
+using JM.ReferenceApplication.Common.Monitoring;
+using System;
+using System.Net.Mail;
+
+namespace JM.Business.Kontakt.Business.Manager
+{
+	[SystemBoundary("CustomerCare", BusinesImpact.Low)]
+	[Intercept(typeof(ErrorInterceptor))]
+	public class KontaktManager : IKontaktManager
+	{
+		public IContactModel GetContactModel()
+		{
+			return new ContactModel();
+		}
+		
+		public void SendContactMail(IContactModel model)
+		{
+			var config = ApplicationConfiguration.GetConfigSection<Config>("JM.Business.Kontakt");
+
+			var mail = new MailMessage(model.Email, config.Mailing.RecipientAdress);
+
+			mail.Subject = String.Format("{0}{1}", config.Mailing.SubjectPrefix,
+				String.IsNullOrEmpty(model.Subject) ? config.Mailing.DefaultSubject : model.Subject);
+
+			mail.Body = String.Format("Mail vom {0}{1}", DateTime.Now, Environment.NewLine);
+			mail.Body += String.Format("Absender: {0} {1} {2} {3}{3}", model.Salutation, model.FirstName, model.LastName, Environment.NewLine);
+			mail.Body += model.Message;
+
+			if (config.Mailing.SendAsHighPriorityMail)
+				mail.Priority = MailPriority.High;
+
+			// Loggen des Aufrufs
+			ApplicationEvents.Log.ContactFormSent(model.Email, model.FirstName, model.LastName);
+
+			var sender = new SmtpClient(config.SmtpServer.Server);
+			sender.Send(mail);
+		}
+
+		private void saveMail(ContactModel model)
+		{
+			
+		}
+	}
+}
---
+++ Work/JM.ReferenceApplication/JM.Business.Kontakt/ModuleDefinition.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using JM.Business.Kontakt.Business.Manager;
+using JM.Business.Kontakt.Contracts.Manager;
+using Autofac.Extras.DynamicProxy2;
+
+namespace JM.Business.Kontakt
+{
+	/// <summary>
+	/// Registriert die Klassen dieser DLL, die für DI über AutoFac zur Verfügung stehen sollen.
+	/// </summary>
+	public class ModuleDefinition : Module
+	{
+		/// <summary>
+		/// Override zum Hinzufügen der Registrationen
+		/// </summary>
+		/// <remarks>
+		/// Der ContainerBuilder-Parameter wird nur für das aktuelle Modul genutzt.
+		/// </remarks>
+		/// <param name="builder">The builder through which components can be registered.</param>
+		protected override void Load(ContainerBuilder builder)
+		{
+			// Basis-Syntax:
+			//builder
+			//	.RegisterType<ExampleImplementation>()
+			//	.As<ExampleInterface>()
+			//	.LifeTimeSetting;
+
+			builder
+				.RegisterType<KontaktManager>()
+				.As<IKontaktManager>()
+				.EnableInterfaceInterceptors()
+				.SingleInstance();
+
+			base.Load(builder);
+		}
+	}
+}
