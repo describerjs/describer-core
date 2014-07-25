076e9541fc9810c34f4198b428c7b1ee7b2b11af
Sebastian van Elten <sve@joinmedia.de>
2014-07-25 16:36:55 +0200

    Area für Demo hinzugefügt Demo für die Testimplementation "Kontaktmanager" hinzugefügt Integration der Testimplementation "Kontaktmanager" aus Web entfernt

---
+++ Work/JM.ReferenceApplication/JM.ReferenceApplication/Areas/JMDemo/Controllers/JM_Business_KontaktController.cs
@@ -0,0 +1,59 @@
+﻿using System.Web.Mvc;
+using JM.Business.Kontakt.Contracts.Manager;
+using JM.ReferenceApplication.Areas.JMDemo.Models.JM.Business._Kontakt;
+
+namespace JM.ReferenceApplication.Areas.JMDemo.Controllers
+{
+	/// <summary>
+	/// 25.07.2014 - Sebastian van Elten
+	/// JM_Business_KontaktController
+	/// </summary>
+	public class JM_Business_KontaktController : Controller
+	{
+		//////////////////////////////////////////////////////////////////////////////////////
+		#region private Member
+
+		private IKontaktManager _kontaktManager;
+
+		#endregion
+
+		//////////////////////////////////////////////////////////////////////////////////////
+		#region Constructor
+
+		public JM_Business_KontaktController(IKontaktManager kontaktManager)
+		{
+			_kontaktManager = kontaktManager;
+		}
+
+		#endregion
+
+		//////////////////////////////////////////////////////////////////////////////////////
+		#region Views
+
+		public ActionResult Index()
+        {
+	        var model = _kontaktManager.GetContactModel();
+            return View(model);
+        }
+
+	    [HttpPost]
+	    public ActionResult Index(ContactModel model)
+	    {
+		    if (!ModelState.IsValid)
+			    return View(model);
+
+			_kontaktManager.SendContactMail(model);
+
+		    return View("Danke", model);
+	    }
+
+		#endregion
+
+		//////////////////////////////////////////////////////////////////////////////////////
+		#region PartialViews
+
+		//
+
+		#endregion
+	}
+}
---
+++ Work/JM.ReferenceApplication/JM.ReferenceApplication/Areas/JMDemo/JMDemoAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using JM.Foundation.Mvc.Routing;
+
+namespace JM.ReferenceApplication.Areas.JMDemo
+{
+    public class JMDemoAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "JMDemo";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            var JMDemo_default = context.MapRoute(
+                "JMDemo_default",
+                "JMDemo/{controller}/{action}/{id}",
+                new { controller = "Start", action = "Index", id = UrlParameter.Optional }
+            );
+			JMDemo_default.RouteHandler = new HyphenatedRouteHandler();
+        }
+    }
+}
