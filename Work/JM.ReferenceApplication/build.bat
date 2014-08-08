56e5ea4c9ae9073084c35c94593caee14957657e
Sebastian van Elten <sve@joinmedia.de>
2014-08-08 12:09:29 +0200

    Build-Bat für FE

---
+++ Work/JM.ReferenceApplication/build.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+
+ECHO.Start Build Basis-Solution
+
+set exefile=%WinDir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe
+%exefile% "JM.ReferenceApplication.sln"
+
+ECHO.**********************************
+ECHO.Fertig mit builden.
+ECHO.Alles ok, keine Fehler?
+CHOICE /C:YN
+
+IF errorlevel ==2 GOTO SOEINMISTEINFEHLER
+IF errorlevel ==1 GOTO ALLESOKMEINFREUND
+
+
+:ALLESOKMEINFREUND
+ECHO.**********************************
+ECHO.Keine Fehler
+ECHO.**********************************
+
+PAUSE
+CLS
+EXIT
+
+:SOEINMISTEINFEHLER
+ECHO.
+ECHO.**********************************
+ECHO.DLLs werden nicht kopiert.
+ECHO.**********************************
+PAUSE
+CLS
+EXIT
