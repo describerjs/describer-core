c1bd977a4fe60ca6caae0b01f6dd32afe9618768
Sebastian van Elten <sve@joinmedia.de>
2014-08-13 10:02:05 +0200

    Partial für Navigation

--- Work/JM.ReferenceApplication/JM.ReferenceApplication/Controllers/HomeController.cs
+++ Work/JM.ReferenceApplication/JM.ReferenceApplication/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Web.Mvc;
 
 namespace JM.ReferenceApplication.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
 }
---
+++ Work/JM.ReferenceApplication/JM.ReferenceApplication/Controllers/NavigationController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+using Piranha.Models;
+
+namespace JM.ReferenceApplication.Controllers
+{
+	/// <summary>
+	/// 13.08.2014 - Sebastian van Elten
+	/// NavigationController
+	/// </summary>
+	public class NavigationController : Controller
+	{
+		//////////////////////////////////////////////////////////////////////////////////////
+		#region private Member
+
+		//
+
+		#endregion
+
+		//////////////////////////////////////////////////////////////////////////////////////
+		#region Constructor
+
+		public NavigationController()
+		{
+
+		}
+
+		#endregion
+
+		//////////////////////////////////////////////////////////////////////////////////////
+		#region Views
+
+		//
+
+		#endregion
+
+		//////////////////////////////////////////////////////////////////////////////////////
+		#region PartialViews
+
+		public PartialViewResult _Navigation()
+		{
+			return PartialView(Sitemap.GetStructure());
+		}
+
+		#endregion
+	}
+}
