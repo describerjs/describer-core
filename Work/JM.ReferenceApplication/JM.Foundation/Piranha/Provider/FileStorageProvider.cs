d3d59423b5d2841f3af11bbe45d9ea61ea6971b5
Tobias Juderjahn <Tobias.Juderjahn@joinmedia.de>
2014-08-18 11:46:33 +0200

    cp

--- Work/JM.ReferenceApplication/JM.Foundation/Piranha/Provider/FileStorageProvider.cs
+++ Work/JM.ReferenceApplication/JM.Foundation/Piranha/Provider/FileStorageProvider.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace JM.Foundation.Piranha.Provider
 {
-    public class FileStorageProvider : global::Piranha.IO.LocalMediaCacheProvider, IMediaCacheProvider
+    public class FileStorageProvider : global::Piranha.IO.LocalMediaProvider
     {
-        public FileStorageProvider() : base(@"c:\temp\PiranhaMedia", @"c:\temp\PiranhaMedia\Content", @"c:\temp\PiranhaMedia\Uploads")
+        public FileStorageProvider() : base(@"c:\temp\PiranhaMedia\Content", @"c:\temp\PiranhaMedia\Uploads")
         {
 
         }
