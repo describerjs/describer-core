75fb7f4851b238428529b9282073326d9c58ebee
Tobias Juderjahn <Tobias.Juderjahn@joinmedia.de>
2014-08-18 13:17:08 +0200

    Debug Mode

--- Work/JM.ReferenceApplication/JM.Foundation/Mvc/Helper/Configuration.cs
+++ Work/JM.ReferenceApplication/JM.Foundation/Mvc/Helper/Configuration.cs
@@ -5,56 +5,69 @@ using System.Web.Mvc;
 
 namespace JM.Foundation.Mvc.Helper
 {
-	/// <summary>
-	/// Dieser Helper bietet dem Frontend Zugriff auf die Konfiguration der Performanceeinstellungen und optionalen Features.
-	/// Die entsprechenden Einstellungen werden in der JM.Foundation - web.config section gepflegt
-	/// </summary>
-	public static class Configuration
-	{
-		//////////////////////////////////////////////////////////////////////////////////////
-		#region public methods
-		
-		/// <summary>
-		/// Prüft ob das übergebene Css-Feature aktiviert ist
-		/// </summary>
-		/// <param name="helper">HtmlHelper</param>
-		/// <param name="feature">Enum-Wert Feature</param>
-		/// <returns>bool</returns>
-		public static bool FeatureEnabled(this HtmlHelper helper, PerformanceFeature feature)
-		{
-			var setting = _getSetting((int)feature);
+    /// <summary>
+    /// Dieser Helper bietet dem Frontend Zugriff auf die Konfiguration der Performanceeinstellungen und optionalen Features.
+    /// Die entsprechenden Einstellungen werden in der JM.Foundation - web.config section gepflegt
+    /// </summary>
+    public static class Configuration
+    {
+        #region public methods
+        
+        /// <summary>
+        /// Prüft ob das übergebene Css-Feature aktiviert ist
+        /// </summary>
+        /// <param name="helper">HtmlHelper</param>
+        /// <param name="feature">Enum-Wert Feature</param>
+        /// <returns>bool</returns>
+        public static bool FeatureEnabled(this HtmlHelper helper, PerformanceFeature feature)
+        {
+            var setting = _getSetting((int)feature);
 
-			return setting != null && setting.Value;
-		}
-		
-		#endregion
+            return setting != null && setting.Value;
+        }
 
-		//////////////////////////////////////////////////////////////////////////////////////
-		#region private methods
+        /// <summary>
+        /// Liefert, ob im DEBUG-Mode
+        /// </summary>
+        public static bool IsDebug
+        {
+            get
+            {
+#if DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+        
+        #endregion
 
-		/// <summary>
-		/// Liest die Configurationsection für JM.Foundation und gibt das entsprechende Config-Objekt zurück
-		/// </summary>
-		/// <returns>Config</returns>
-		private static Config _getConfig()
-		{
-			return ApplicationConfiguration.GetConfigSection<Config>("JM.Foundation");
-		}
+        #region private methods
 
-		/// <summary>
-		/// Holt ein bestimmtes Setting anhand der ID
-		/// </summary>
-		/// <param name="id">Id des Settings</param>
-		/// <returns>Setting</returns>
-		private static Setting _getSetting(int id)
-		{
-			var config = _getConfig();
+        /// <summary>
+        /// Liest die Configurationsection für JM.Foundation und gibt das entsprechende Config-Objekt zurück
+        /// </summary>
+        /// <returns>Config</returns>
+        private static Config _getConfig()
+        {
+            return ApplicationConfiguration.GetConfigSection<Config>("JM.Foundation");
+        }
+
+        /// <summary>
+        /// Holt ein bestimmtes Setting anhand der ID
+        /// </summary>
+        /// <param name="id">Id des Settings</param>
+        /// <returns>Setting</returns>
+        private static Setting _getSetting(int id)
+        {
+            var config = _getConfig();
             
             return 
                 (config.Features ?? Enumerable.Empty<Setting>())
                 .FirstOrDefault(d => d.ID == id.ToString());
-		}
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
