5633a6aa8bb151060dae1bb8184a482796af7c6e
Tobias Juderjahn <Tobias.Juderjahn@joinmedia.de>
2014-08-19 17:54:19 +0200

    s

--- Buildsystem/BuildProcess/Versionierung_JS_CSS.cmd
+++ Buildsystem/BuildProcess/Versionierung_JS_CSS.cmd
@@ -1,3 +1,3 @@
-Path %Path%;C:\Users\Administrator\AppData\Roaming\npm
 CD "%1"
-"c:\Program Files\nodejs\node.exe" grunt
+Path %Path";C:\Users\Administrator\AppData\Roaming\npm
+"C:\Program Files\nodejs\node.exe" c:\npm\node_modules\grunt-cli\bin\grunt
