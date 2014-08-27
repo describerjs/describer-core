529cf0502224254496d40f883a804c70ce487957
Tobias Juderjahn <Tobias.Juderjahn@joinmedia.de>
2014-08-27 10:28:03 +0200

    Wizard

--- JM.BaseSolutionWizard/JM.BaseSolutionWizard/InstallWizard.cs
+++ JM.BaseSolutionWizard/JM.BaseSolutionWizard/InstallWizard.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TemplateWizard;
 using EnvDTE;
+using System.IO;
 
 namespace JM.BaseSolutionWizard
 {
@@ -37,6 +38,37 @@ namespace JM.BaseSolutionWizard
         public bool ShouldAddProjectItem(string filePath)
         {
             return true;
+        }
+
+        /// <summary>
+        /// Liefert den Pfad der erstellten Solution.
+        /// </summary>
+        /// <returns></returns>
+        private string GetSolutionRootPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="filePath">Z.B. C:\Dev\NeueSolution</param>
+        /// <param name="solutionFolder">Z.B. "Solution Files\Environments\Dev</param>
+        /// <remarks>Erstellt den Solution Folder falls nicht vorhanden. Auch rekursiv.</remarks>
+        private void AddFileToSolutionFolder(string filePath, string solutionFolder)
+        {
+
+        }
+
+        /// <summary>
+        /// Liefert einen lesenden Filestream auf eine Datei, die sich in der template.zip befindet.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        /// <remarks>Die Datei muss sich im Root-Verzeichnis des Templates befinden.</remarks>
+        private FileStream GetFilestreamFromTemplate(string filename)
+        {
+            throw new NotImplementedException();
         }
     }
 }
