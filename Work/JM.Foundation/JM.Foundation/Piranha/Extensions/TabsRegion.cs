d3f1e844607d2c6e79b08fc4f09463b93b47afa6
Sebastian van Elten <sve@joinmedia.de>
2014-08-28 17:02:14 +0200

    Erster Entwurf Reiter-Element für Piranha

---
+++ Work/JM.Foundation/JM.Foundation/Piranha/Extensions/TabsRegion.cs
@@ -0,0 +1,16 @@
+﻿using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Piranha.Extend.Regions;
+using Extension = Piranha.Extend.Extension;
+
+    [Export(typeof(IExtension))]
+    [ExportMetadata("InternalId", "TabsRegion")]
+	[ExportMetadata("Name", "TabsRegion")]
+    [ExportMetadata("Type", ExtensionType.Region)]
+    [Serializable]
+	public class TabsRegion : Extension
+    {
+		public IList<TextRegion> Tabs { get; set; }
+    }
