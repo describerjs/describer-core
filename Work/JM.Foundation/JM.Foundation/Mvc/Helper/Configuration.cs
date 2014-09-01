371ba870ecea3f55947492b974eb57cebc825e47
Sebastian van Elten <sve@joinmedia.de>
2014-09-01 13:37:04 +0200

    Korrektur Piranha-Reiter Modul

--- Work/JM.Foundation/JM.Foundation/Mvc/Helper/Configuration.cs
+++ Work/JM.Foundation/JM.Foundation/Mvc/Helper/Configuration.cs
@@ -1,5 +1,4 @@
 ﻿using JM.Foundation.Configuration;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
---
+++ Work/JM.Foundation/JM.Foundation/Mvc/Helper/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace JM.Foundation.Mvc.Helper
+{
+	public static class Helper
+	{
+		//////////////////////////////////////////////////////////////////////////////////////
+		#region extensions für mvc html helpers
+
+		public static string GetFullHtmlFieldName<TModel, TProperty>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression)
+		{
+			return helper.ViewData.TemplateInfo.GetFullHtmlFieldName(expression);
+		}
+
+		public static string GetFullHtmlFieldId<TModel, TProperty>(this HtmlHelper<TModel> helper, Expression<Func<TModel, TProperty>> expression)
+		{
+			return helper.ViewData.TemplateInfo.GetFullHtmlFieldId(expression);
+		}
+
+		public static string GetFullHtmlFieldName<TModel, TProperty>(this TemplateInfo templateInfo, Expression<Func<TModel, TProperty>> expression)
+		{
+			return templateInfo.GetFullHtmlFieldName(ExpressionHelper.GetExpressionText(expression));
+		}
+
+		public static string GetFullHtmlFieldId<TModel, TProperty>(this TemplateInfo templateInfo, Expression<Func<TModel, TProperty>> expression)
+		{
+			return templateInfo.GetFullHtmlFieldId(ExpressionHelper.GetExpressionText(expression));
+		}
+
+		#endregion
+	}
+}
