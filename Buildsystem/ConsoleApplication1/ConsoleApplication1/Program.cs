62ec0609481c5cba32715bc34175b2392451aaf0
Tobias Juderjahn <Tobias.Juderjahn@joinmedia.de>
2014-09-15 17:18:58 +0200

    test

--- Buildsystem/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Buildsystem/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Microsoft.TeamFoundation.VersionControl.Client.
+            Microsoft.TeamFoundation.VersionControl.Clie
         }
     }
 }
