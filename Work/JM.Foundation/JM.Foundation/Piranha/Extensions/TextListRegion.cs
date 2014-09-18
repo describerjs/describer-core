b669d8e77bc6f7c224a8e488415b6d8b89fdcd82
Felipe Hidalgo <Felipe.Hidalgo@joinmedia.de>
2014-09-18 11:17:32 +0200

    TextListRegion

---
+++ Work/JM.Foundation/JM.Foundation/Piranha/Extensions/TextListRegion.cs
@@ -0,0 +1,16 @@
+﻿using Piranha.Extend;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Piranha.Extend.Regions;
+using Extension = Piranha.Extend.Extension;
+
+[Export(typeof(IExtension))]
+[ExportMetadata("InternalId", "TextListRegion")]
+[ExportMetadata("Name", "TextListRegion")]
+[ExportMetadata("Type", ExtensionType.Region)]
+[Serializable]
+public class TextListRegion : Extension
+{
+    public IList<TextRegion> TextList { get; set; }
+}
