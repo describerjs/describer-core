3b3530392ea2795afe7a88d6239ece22d8c023c3
Sebastian van Elten <sve@joinmedia.de>
2014-10-13 15:44:54 +0200

    Pfad zur MediaStorage auf Rex angepasst (IP statt Name)

--- Work/JM.Foundation/JM.Foundation/Piranha/Provider/FileStorageProvider.cs
+++ Work/JM.Foundation/JM.Foundation/Piranha/Provider/FileStorageProvider.cs
@@ -1,16 +1,10 @@
-﻿using Piranha.IO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace JM.Foundation.Piranha.Provider
 {
     public class FileStorageProvider : global::Piranha.IO.LocalMediaProvider
     {
         public FileStorageProvider()
-            : base(@"\\REX\PiranhaMedia\Content", @"\\REX\PiranhaMedia\Uploads")
+			: base(@"\\172.30.106.1\PiranhaMedia\Content", @"\\172.30.106.1\PiranhaMedia\Uploads")
         {
         }
     }
