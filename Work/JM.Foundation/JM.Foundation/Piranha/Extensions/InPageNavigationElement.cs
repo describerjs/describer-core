43ab08ac508600f2bf3551a9de8c3a82bb108d0e
Sebastian van Elten <sve@joinmedia.de>
2014-10-14 12:43:44 +0200

    Neues Element für die OnePagenavigation

---
+++ Work/JM.Foundation/JM.Foundation/Piranha/Extensions/InPageNavigationElement.cs
@@ -0,0 +1,14 @@
+﻿using Piranha.Extend;
+using System;
+using System.ComponentModel.Composition;
+using Extension = Piranha.Extend.Extension;
+
+    [Export(typeof(IExtension))]
+	[ExportMetadata("InternalId", "InPageNavigationElement")]
+	[ExportMetadata("Name", "InPageNavigationElement")]
+    [ExportMetadata("Type", ExtensionType.Region)]
+    [Serializable]
+	public class InPageNavigationElement : Extension
+    {
+        public string Text { get; set; }
+    }
