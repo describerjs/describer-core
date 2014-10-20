89a5e53a3611fb16c8cb8e2ed9e84e49681b11d8
Sebastian van Elten <sve@joinmedia.de>
2014-10-20 15:49:38 +0200

    Neue Region "OnePageSection" angelegt

---
+++ Work/JM.Foundation/JM.Foundation/Piranha/Extensions/OnePageSection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Piranha.Extend;
+using Piranha.Extend.Regions;
+
+[Export(typeof(IExtension))]
+[ExportMetadata("InternalId", "OnePageSection")]
+[ExportMetadata("Name", "OnePageSection")]
+[ExportMetadata("Type", ExtensionType.Region)]
+[Serializable]
+public class OnePageSection : Extension
+{
+    public string Headline
+    {
+        get;
+        set;
+    }
+    
+    public string Content
+    {
+        get;
+        set;
+    }
+
+    public string NavigationName
+    {
+        get;
+        set;
+    }
+
+	public ImageRegion Image { get; set; }
+}
