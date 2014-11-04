c4c90cea9b8511da6af150c247891f0141d6d438
Sebastian van Elten <sve@joinmedia.de>
2014-11-04 09:15:00 +0100

    Neue Region "AnimatedText" für die Darstellung von Textelementen mit OnScroll-Animation

---
+++ Work/JM.Foundation/JM.Foundation/Piranha/Extensions/AnimatedText.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Web.Mvc;
+using Piranha.Extend;
+using Piranha.Extend.Regions;
+
+[Export(typeof(IExtension))]
+[ExportMetadata("InternalId", "AnimatedText")]
+[ExportMetadata("Name", "AnimatedText")]
+[ExportMetadata("Type", ExtensionType.Region)]
+[Serializable]
+public class AnimatedText : Extension
+{
+	private string _textEffect;
+
+	public string Content { get; set; }
+
+	public bool Hide { get; set; }
+
+	public string TextEffect
+	{
+		get { return _textEffect ?? (_textEffect = string.Empty); }
+		set { _textEffect = value; }
+	}
+
+	public List<SelectListItem> TextEffectSelection
+	{
+		get
+		{
+			return new List<SelectListItem>
+			{
+				new SelectListItem { Text = "Kein Effekt", Value = string.Empty },
+				new SelectListItem { Text = "Fade-In / Out", Value = "fade-in" },
+				new SelectListItem { Text = "Slide-In / Out", Value = "slide-in" }
+			};
+		}
+	}
+}
