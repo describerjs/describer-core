b318659bb7ec1faf943a7fdcb3f59bbe6a738315
Sebastian van Elten <sve@joinmedia.de>
2014-11-05 15:23:24 +0100

    AnimatedImage neue Dateien

---
+++ Work/JM.Foundation/JM.Foundation/Piranha/Models/ImageAnimationSelection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace JM.Foundation.Piranha.Models
+{
+	public static class ImageAnimationSelection
+	{
+		public static List<SelectListItem> Get()
+		{
+			return new List<SelectListItem>
+			{
+				new SelectListItem {Text = "Kein Effekt", Value = ""},
+				new SelectListItem {Text = "Parallax", Value = "parallax"},
+				new SelectListItem {Text = "Gyroskop", Value = "gyroscope"},
+				new SelectListItem {Text = "Parallax + Gyroskop", Value = "parallax_gyroscope"},
+			};
+		}
+	}
+}
