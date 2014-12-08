2c52deb2c5a2131dd36d83a14234309b134a87da
Andreas Otten <ao@joinmedia.de>
2014-12-08 14:24:38 +0100

    Squashed 'joinmedia/js/describer-more/' changes from 545a1c8..1ef9419
    1ef9419 fixed and extended  onView.js
    abc44f5 add custom window.innerHeight; add dc-orientationchange-Event on body
    
    git-subtree-dir: joinmedia/js/describer-more
    git-subtree-split: 1ef9419bc26292fd0b095231e08263617a4e2787

--- modules/onView.js
+++ modules/onView.js
@@ -25,34 +25,63 @@ define(['jquery', '_super'], function ($, _super){
 		},
 
 		onInitExec: function () {
-			this.eot = this.$elem.offset().top;
-			this.offset = 0;//window.innerHeight * 0.2;
-			this.elemHeight = this.$elem[0].getBoundingClientRect().height;
-			this.dynamic = (this.is('heigth') === 'dynamic');
-			this.$destination = this.getRelatedToElem();
-			this.onlyTopOfElem = (this.is('onlyTopOfElem') === 'true');
+			this.setProperties();
+			this.orientation = (window.innerHeight > window.innerWidth) ? 'p':'w';
+			$(window).on('resize', this.checkOrientation.bind(this));
 		},
 
 		options: {
 
 		},
 
+		checkOrientation: function(){
+			if(this.orientation === 'w' && (window.innerHeight > window.innerWidth)){
+				this.orientation = 'p';
+				this.setProperties.bind(this);
+			}else if(this.orientation === 'p' && (window.innerHeight < window.innerWidth)){
+				this.orientation = 'w';
+				this.setProperties.bind(this);
+			}
+		},
+
+		setProperties: function(){
+			this.eot = this.$elem.offset().top;
+			this.offsetTop = (this.is('offsetTop') !== '') ? parseInt(this.is('offsetTop').replace('px', ''), 10) : 0;//window.innerHeight * 0.2;
+			this.offsetBottom = (this.is('offsetBottom') !== '') ? parseInt(this.is('offsetBottom').replace('px', ''), 10) : 0;//window.innerHeight * 0.2;
+			this.elemHeight = this.$elem[0].getBoundingClientRect().height;
+			this.dynamic = (this.is('heigth') === 'dynamic');
+			this.$destination = this.getRelatedToElem();
+			this.onlyTopOfElem = (this.is('onlyTopOfElem') === 'true');
+		},
+
 		_exec: function(e){
 			var _viewportTop = window.dc.win.pageYOffset;
 			var _viewportBottom = _viewportTop + window.dc.win.innerHeight;
 			if(jmHF.hasClass(this.$destination[0], 'show')){
-				if((!(_viewportBottom > this.eot + this.offset)) && (_viewportTop < (this.eot + ((this.dynamic) ? this.$elem[0].getBoundingClientRect().height : this.elemHeight)))){
-					this.$destination.removeClass('show');
-					this._finishing();
+				if(this.onlyTopOfElem){
+					if(_viewportBottom < this.eot + this.offsetTop){
+						this.$destination.removeClass('show');
+						this._finishing();
+					}
+					/*if((!(_viewportBottom > this.eot + this.offsetTop)) && (_viewportTop < (this.eot + ((this.dynamic) ? this.$elem[0].getBoundingClientRect().height : this.elemHeight) + this.offsetBottom))){
+						this.$destination.removeClass('show');
+						this._finishing();
+					}*/
+				}else{
+					if((_viewportBottom < this.eot + this.offsetTop) || (_viewportTop > (this.eot + ((this.dynamic) ? this.$elem[0].getBoundingClientRect().height : this.elemHeight) + this.offsetBottom))){
+						this.$destination.removeClass('show');
+						this._finishing();
+					}
 				}
+
 			}else{
 				if(this.onlyTopOfElem){
-					if(_viewportBottom > this.eot + this.offset){
+					if(_viewportBottom > this.eot + this.offsetTop){
 						this.$destination.addClass('show');
 						this._finishing();
 					}
 				}else{
-					if((_viewportBottom > this.eot + this.offset) && (_viewportTop < (this.eot + ((this.dynamic) ? this.$elem[0].getBoundingClientRect().height : this.elemHeight)))){
+					if((_viewportBottom > this.eot + this.offsetTop) && (_viewportTop < (this.eot + ((this.dynamic) ? this.$elem[0].getBoundingClientRect().height : this.elemHeight) + this.offsetBottom))){
 						this.$destination.addClass('show');
 						this._finishing();
 					}
