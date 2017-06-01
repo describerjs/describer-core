39daee99ea8006a4ba9996dc4ac1b11dc872dad0
Daniel Zöllmann <dz@joinmedia.de>
2017-06-01 15:58:51 +0200

    ^ compiled

--- dc_core.js
+++ dc_core.js
@@ -1,1 +1,4 @@
-//# sourceMappingURL=dc_core.js.map
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJkZXNjcmliZXItY29yZS9kY19jb3JlLmpzIiwic291cmNlc0NvbnRlbnQiOltdfQ==
